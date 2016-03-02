1d1016c93098994a20002aabacea3982792bce7e
Hunor Kovács <hunor.kovacs@ft.com>
2016-03-02 13:56:16 +0200

    Adding logging and environmentvar and flag reading libs.

--- notifications-push.go
+++ notifications-push.go
@@ -3,14 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"github.com/jawher/mow.cli"
+	"os"
+"io"
 )
 
-func main() {
-	dispatcher := NewEvents()
-	go dispatcher.receiveEvents()
-	go dispatcher.distributeEvents()
+const logPattern = log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile | log.LUTC
 
-	controller := Controller{dispatcher}
-	http.HandleFunc("/", controller.handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+var infoLogger *log.Logger
+var warnLogger *log.Logger
+var errorLogger *log.Logger
+
+func main() {
+	app := cli.App("notifications-push", "Proactively notifies subscribers about new content publishes/modifications.")
+	socksProxy := app.String(cli.StringOpt{
+		Name:   "socks-proxy",
+		Value:  "",
+		Desc:   "Use specified SOCKS proxy (e.g. localhost:2323)",
+		EnvVar: "SOCKS_PROXY",
+	})
+	println(socksProxy)
+	app.Action = func() {
+		initLogs(os.Stdout, os.Stdout, os.Stderr)
+
+		dispatcher := NewEvents()
+		go dispatcher.receiveEvents()
+		go dispatcher.distributeEvents()
+
+		controller := Controller{dispatcher}
+		http.HandleFunc("/", controller.handler)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+	app.Run(os.Args)
+}
+
+func initLogs(infoHandle io.Writer, warnHandle io.Writer, errorHandle io.Writer) {
+	infoLogger = log.New(infoHandle, "INFO  - ", logPattern)
+	warnLogger = log.New(warnHandle, "WARN  - ", logPattern)
+	errorLogger = log.New(errorHandle, "ERROR - ", logPattern)
 }

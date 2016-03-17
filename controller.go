1574676d7e670b714d3655f9d190924f5501590f
Zoltan Bodor <Zoltan.Bodor@ft.com>
2016-03-17 11:25:11 +0200

    detect closed connections

--- controller.go
+++ controller.go
@@ -10,37 +10,47 @@ type Controller struct {
 }
 
 func (c Controller) notifications(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Pragma", "no-cache")
 	w.Header().Set("Expires", "0")
 
+	cn, ok := w.(http.CloseNotifier)
+	if !ok {
+		http.Error(w, "Cannot stream.", http.StatusInternalServerError)
+		return
+	}
+
 	bw := bufio.NewWriter(w)
 
 	events := make(chan string)
-
 	c.dispatcher.addSubscriber <- events
 	defer func() {
 		c.dispatcher.removeSubscriber <- events
 	}()
+
 	for {
-		_, err := bw.WriteString(<-events)
-		if err != nil {
-			infoLogger.Printf("[%v]", err)
+		select {
+		case <-cn.CloseNotify():
 			return
+		case event := <-events:
+			_, err := bw.WriteString(event)
+			if err != nil {
+				infoLogger.Printf("[%v]", err)
+				return
+			}
+			err = bw.WriteByte('\n')
+			if err != nil {
+				infoLogger.Printf("[%v]", err)
+				return
+			}
+			err = bw.Flush()
+			if err != nil {
+				infoLogger.Printf("[%v]", err)
+				return
+			}
+			flusher := w.(http.Flusher)
+			flusher.Flush()
 		}
-		err = bw.WriteByte('\n')
-		if err != nil {
-			infoLogger.Printf("[%v]", err)
-			return
-		}
-		err = bw.Flush()
-		if err != nil {
-			infoLogger.Printf("[%v]", err)
-			return
-		}
-		flusher := w.(http.Flusher)
-		flusher.Flush()
 	}
 }

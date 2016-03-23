ad85ecd09f08d1830a6be2585eb82de74bb99a37
Zoltan Bodor <Zoltan.Bodor@ft.com>
2016-03-23 09:52:00 +0200

    store client address also for direct connections

--- controller_test.go
+++ controller_test.go
@@ -3,17 +3,32 @@ package main
 import (
 	"net/http"
 	"testing"
-	"time"
 )
 
-func TestBuildSubscriber(t *testing.T) {
+func TestGetClientAddr_XForwardedHeadersPopulated(t *testing.T) {
 	testHeaders := http.Header{}
 	testHeaders["X-Forwarded-For"] = []string{"1.2.3.4", "5.6.7.8"}
 	testHeaders["X-Forwarded-Port"] = []string{"12345"}
 
-	subscriber := buildSubscriber(testHeaders)
+	testRequest := &http.Request{
+		Header: testHeaders,
+	}
 
-	if subscriber.addr != "1.2.3.4:12345" || subscriber.since.After(time.Now()) {
-		t.Error("Expected success!")
+	addr := getClientAddr(testRequest)
+
+	if addr != "1.2.3.4:12345" {
+		t.Errorf("Expected: [1.2.3.4:12345]. Actual: [%v]", addr)
+	}
+}
+
+func TestGetClientAddr_XForwardedHeadersMissing(t *testing.T) {
+	testRequest := &http.Request{
+		RemoteAddr: "10.10.10.10:10101",
+	}
+
+	addr := getClientAddr(testRequest)
+
+	if addr != "10.10.10.10:10101" {
+		t.Error("Expected: [10.10.10.10:10101]. Actual: [%v]", addr)
 	}
 }

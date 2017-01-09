b5da028701c76e9cf8eb750f4c79cf4b3e5db885
Panziera, Luca <luca.panziera@ft.com>
2017-01-09 16:33:37 +0000

    Updated panic guide URL and exported functions description of the healthcheck

--- resources/healthchecks.go
+++ resources/healthchecks.go
@@ -13,11 +13,14 @@ import (
 	"github.com/Financial-Times/message-queue-gonsumer/consumer"
 )
 
+//NotificationsPushHealthcheck is the main health check for the notifications-push service
 type NotificationsPushHealthcheck struct {
 	Client         *http.Client
 	ConsumerConfig consumer.QueueConfig
 }
 
+// NewNotificationsPushHealthcheck returns a new instance of NotificationsPushHealthcheck.
+// It requires the configuration of the consumer queue.
 func NewNotificationsPushHealthcheck(consumerConfig consumer.QueueConfig) *NotificationsPushHealthcheck {
 	return &NotificationsPushHealthcheck{
 		Client:         &http.Client{},
@@ -25,17 +28,19 @@ func NewNotificationsPushHealthcheck(consumerConfig consumer.QueueConfig) *Notif
 	}
 }
 
+// GTG is the HTTP handler function for the good-to-go endpoint of notifications-push
 func (hc *NotificationsPushHealthcheck) GTG(w http.ResponseWriter, req *http.Request) {
 	if _, err := hc.checkAggregateMessageQueueProxiesReachable(); err != nil {
 		w.WriteHeader(http.StatusServiceUnavailable)
 	}
 }
 
+// Check is the the NotificationsPushHealthcheck method that checks is the queue proxies are available
 func (hc *NotificationsPushHealthcheck) Check() fthealth.Check {
 	return fthealth.Check{
 		BusinessImpact:   "Notifications about newly modified/published content will not reach this app, nor will they reach its clients.",
 		Name:             "MessageQueueProxyReachable",
-		PanicGuide:       "https://sites.google.com/a/ft.com/technology/systems/dynamic-semantic-publishing/extra-publishing/notifications-push-runbook",
+		PanicGuide:       "https://sites.google.com/a/ft.com/universal-publishing/ops-guides/notifications-push",
 		Severity:         1,
 		TechnicalSummary: "Message queue proxy is not reachable/healthy",
 		Checker:          hc.checkAggregateMessageQueueProxiesReachable,
